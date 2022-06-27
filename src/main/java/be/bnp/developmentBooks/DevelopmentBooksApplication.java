57dd88142ffec7fb56865fcb8a1a054a59224b1b
dscohier <dorian.scohier@gmail.com>
2022-06-27 11:09:29 +0200

    Config for spring boot

---
+++ src/main/java/be/bnp/developmentBooks/DevelopmentBooksApplication.java
@@ -0,0 +1,13 @@
+package be.bnp.developmentBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/bnp/developmentBooks/DevelopmentBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bnp.developmentBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
