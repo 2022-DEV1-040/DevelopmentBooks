75a90c04124650bef64addf4f4c909ae1f7187a0
dscohier <dorian.scohier@gmail.com>
2022-06-27 11:56:59 +0200

    Add h2 configuration + script db for add books

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (id, name) VALUES (1, 'Clean Code (Robert Martin, 2008)');
+INSERT INTO books (id, name) VALUES (2, 'The Clean Coder (Robert Martin, 2011)');
+INSERT INTO books (id, name) VALUES (3, 'Clean Architecture (Robert Martin, 2017)');
+INSERT INTO books (id, name) VALUES (4, 'Test Driven Development by Example (Kent Beck, 2003)');
+INSERT INTO books (id, name) VALUES (5, 'Working Effectively With Legacy Code (Michael C. Feathers, 2004)');
